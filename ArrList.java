ca951565d3f6db72cf68991e86eea028ac47f902
aditi <sharmaaditi7505@gmail.com>
2023-09-20 13:43:47 +0530

    -

--- ArrList.java
+++ ArrList.java
@@ -3,9 +3,13 @@ public class ArrList {
     public static void main(String[] args){
         ArrayList<Integer> arr=new ArrayList<>();
         arr.add(1);
+        arr.add(1);
+        arr.add(1);
+        arr.add(1);
+        arr.add(2,2);// to add at an index
         System.out.println(arr);
-        arr.get(0);
-        arr.remove(0);
+        arr.get(0);//to get an element
+        arr.remove(0);//to remove
         System.out.print(arr);
 
     }
